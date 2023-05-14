68dc6dfdb7bc7fbb70285f4f34558913b150e3da
Youssef Nour <81862730+YoussefNour1@users.noreply.github.com>
2023-05-15 00:24:48 +0300

    first

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return ['Laravel' => app()->version()];
 });
-
+Route::get('/link', function (){
+    return \Illuminate\Support\Facades\Artisan::call('migrate');
+});
 require __DIR__.'/auth.php';
